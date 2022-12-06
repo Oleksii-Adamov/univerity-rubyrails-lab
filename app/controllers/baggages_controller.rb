a1bec853369a84ba00e85b09007002b8024f3476
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-12-06 20:58:11 +0200

    implemented uptade, destroy, task a

--- app/controllers/baggages_controller.rb
+++ app/controllers/baggages_controller.rb
@@ -1,4 +1,11 @@
 class BaggagesController < ApplicationController
+
+  def initialize
+    @mean_num = Float::NAN
+    @mean_weight = Float::NAN
+    @baggages = nil
+  end
+
   def index
     @baggages = Baggage.all
   end
@@ -35,8 +42,51 @@ class BaggagesController < ApplicationController
     end
   end
 
+  def destroy
+    @baggage = Baggage.find(params[:id])
+    @baggage.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
-    def baggage_params
-      params.require(:baggage).permit(:num, :weight)
+
+  def baggage_params
+    params.require(:baggage).permit(:num, :weight)
+  end
+
+  def mean_weight
+    if @mean_num.nan?
+      sum = @baggages.reduce(0) {|acc, baggage| acc + baggage.weight}
+      @mean_num = sum .fdiv @baggages.length
     end
+    @mean_num
+  end
+
+  def mean_num
+    if @mean_weight.nan?
+      sum = @baggages.reduce(0) {|acc, baggage| acc + baggage.num}
+      sum .fdiv @baggages.length
+    end
+    @mean_weight
+  end
+
+  def baggage_mean_weight(baggage)
+    @weight .fdiv @num
+  end
+
+  public
+
+  def c_task
+    delta = 0.5
+    result = (not @baggages.find {|baggage| @baggages.select{|second_baggage| baggage.num == second_baggage.num and
+      not (baggage.weight - second_baggage.weight).abs > delta}.length > 0}.nil?)
+    flash.now[:notice] = "True" ? result : "False"
+  end
+
+  def a_task
+    delta = 0.3
+    #res_baggage = @baggages.find {|baggage| not (baggage_mean_weight(baggage) - mean).abs > delta}
+    @baggage = Baggage.where("ABS(weight - :mean) > :delta", {mean: Baggage.average(:weight), delta: delta}).first
+  end
 end
