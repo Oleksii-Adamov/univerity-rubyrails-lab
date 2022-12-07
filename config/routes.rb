5bdb63be74670ee80fb82cf5acb3f1d0ea839c24
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-12-08 00:16:13 +0200

    added localization using I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'baggages/all_baggage'
-  get 'baggages/b_task'
-  get 'baggages/c_task'
-  get 'baggages/d_task'
-  get 'baggages/e_task'
-  get 'baggages/a_task'
-  get 'baggages/index'
-  root "baggages#index"
-  resources :baggages
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'baggages/all_baggage'
+    get 'baggages/b_task'
+    get 'baggages/c_task'
+    get 'baggages/d_task'
+    get 'baggages/e_task'
+    get 'baggages/a_task'
+    get 'baggages/index'
+    root "baggages#index"
+    resources :baggages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
